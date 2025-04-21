6eae46508f8aef4f8d19300deb218f131a8f411f
claytongit <claytontavares61@gmail.com>
2025-04-21 11:52:17 -0300

    create models and setting database

---
+++ models/Supplier.php
@@ -0,0 +1,68 @@
+<?php
+require_once "config/database.php";
+
+class Supplier {
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::connect();
+    }
+
+    public function getAll() {
+        $stmt = $this->db->query("SELECT * FROM suppliers ORDER BY name ASC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function search($name) {
+        $stmt = $this->db->prepare("SELECT * FROM suppliers WHERE name LIKE :name");
+        $stmt->bindValue(':name', "%$name%");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id) {
+        $stmt = $this->db->prepare("SELECT * FROM suppliers WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($name, $description, $cnpj, $cep, $andress, $number, $city, $state) {
+        $stmt = $this->db->prepare("INSERT INTO suppliers (name, description, cnpj, cep, andress, number, city, state) VALUES (:name, :description, :cnpj, :cep, :andress, :number, :city, :state)");
+        $stmt->bindValue(':name', $name);
+        $stmt->bindValue(':description', $description);
+        $stmt->bindValue(':cnpj', $cnpj);
+        $stmt->bindValue(':cep', $cep);
+        $stmt->bindValue(':andress', $andress);
+        $stmt->bindValue(':number', $number);
+        $stmt->bindValue(':city', $city);
+        $stmt->bindValue(':state', $state);
+        return $stmt->execute();
+    }
+
+    public function update($id, $name) {
+        $stmt = $this->db->prepare("UPDATE suppliers SET name = :name, description = :description, cnpj = :cnpj, cep = :cep, andress = :andress, number = :number, city = :city, state = :state WHERE id = ?");
+        $stmt->bindValue(':name', $name);
+        $stmt->bindValue('description', $description);
+        $stmt->bindValue('cnpj', $cnpj);
+        $stmt->bindValue('cep', $cep);
+        $stmt->bindValue('andress', $andress);
+        $stmt->bindValue('number', $number);
+        $stmt->bindValue('city', $city);
+        $stmt->bindValue('state', $state);
+        $stmt->bindValue('id', $id);
+        return $stmt->execute();
+    }
+
+    public function delete($id) {
+        $stmt = $this->db->prepare("DELETE FROM suppliers WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        return $stmt->execute();
+    }
+
+    public function deleteMultiple($ids) {
+        $in = str_repeat('?,', count($ids) - 1) . '?';
+        $stmt = $this->db->prepare("DELETE FROM suppliers WHERE id IN ($in)");
+        return $stmt->execute($ids);
+    }
+}
